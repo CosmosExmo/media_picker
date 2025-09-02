a80ddf1d25be44040b5aae1bbdf364c55214fffc
CosmosExmo <48317781+CosmosExmo@users.noreply.github.com>
2025-09-02 11:33:30 +0300

    chore: update package versions and dependencies
    - Bumped package version to 0.11.1 in pubspec.yaml and updated the package version in code.
    - Updated dependencies in pubspec.lock, including args, async, camerawesome, and others.
    - Adjusted the Flutter SDK version requirement to >=3.29.0.

--- lib/src/utils/get_package_info.dart
+++ lib/src/utils/get_package_info.dart
@@ -1,5 +1,5 @@
 class PackageInfoHolder {
   //TODO: Read package version from pubspec.yaml
-  final String _packageVersion = "v0.11.0";
+  final String _packageVersion = "v0.11.1";
   String get packageVersion => _packageVersion;
 }
