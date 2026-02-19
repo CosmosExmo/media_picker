d3c20f5451c8aa7c36d893615f7fda2909453903
Kaan Cetinel <velikaancetinel@destechhasar.com>
2026-02-19 12:24:36 +0300

    feat: Add a comprehensive example application for the media_picker package and integrate native device orientation handling into the camera view.

---
+++ example/android/app/src/main/kotlin/com/example/mediapicker/example/LandscapeApplication.kt
@@ -0,0 +1,24 @@
+package com.example.mediapicker.example
+
+import android.app.Activity
+import android.app.Application
+import android.content.pm.ActivityInfo
+import android.os.Bundle
+import io.flutter.app.FlutterApplication
+
+class LandscapeApplication : FlutterApplication() {
+    override fun onCreate() {
+        super.onCreate()
+        registerActivityLifecycleCallbacks(object : ActivityLifecycleCallbacks {
+            override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+                activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE
+            }
+            override fun onActivityStarted(activity: Activity) {}
+            override fun onActivityResumed(activity: Activity) {}
+            override fun onActivityPaused(activity: Activity) {}
+            override fun onActivityStopped(activity: Activity) {}
+            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
+            override fun onActivityDestroyed(activity: Activity) {}
+        })
+    }
+}
